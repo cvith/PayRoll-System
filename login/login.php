b7dcf33c9fc61a081c0a225ccaca6a82bfdc02c8
CVITH\vithanage <kvithanage25@gmail.com>
2023-03-01 13:36:20 +0530

    first commit

---
+++ login/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login page</title>
+</head>
+<body>
+    <div class="panel">
+        <div class="panel_left">
+            <div class="header_text">
+                PayRoll System
+            </div>
+            <div class="body_text">
+                Manage your salary sheet from here, in an efficient and effective way...
+            </div>
+        </div>
+
+        <div class="panel_right">
+            <div class="login_panel">
+                <h1>Login</h1>
+                <div class="input_wrap">
+                    <label>username</label>
+                    <br>
+                    <input type="text" class="input_field">
+                </div>
+                <div class="input_wrap">
+                    <label>password</label>
+                    <br>
+                    <input type="password" class="input_field">
+                </div>
+                <div class="login_btn">
+                    <button>login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
